5d1471a9607eea5937207365bda7241cb4c27b81
Maksim <66447337+LGHTM@users.noreply.github.com>
2020-06-11 15:57:32 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { render } from "@testing-library/react";
+
+class PokemonForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pokemonForms: null,
+    };
+  }
+  async componentDidMount() {
+    const reponse = await fetch("https://pokeapi.co/api/v2/pokemon-form");
+    const pokemonForms = await reponse.json();
+    console.log(pokemonForms)
+    this.setState({
+      pokemonForms: pokemonForms.results,
+    });
+  }
+  render() {
+    if (this.state.pokemonForms === null) {
+      return <p>...Loading...</p>;
+    }
+    return (
+      <ul>
+        {this.state.pokemonForms.map((type) => (
+          <li key={type.id}>{type.name}</li>
+        ))}
+      </ul>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <PokemonForm/>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
